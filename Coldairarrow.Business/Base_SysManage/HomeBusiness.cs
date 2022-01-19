c1951a436aad5b06332da497f3ac8d956b293479
Carlos Carvajal <aos.ccarvajal@fogafin.gov.co>
2022-01-18 20:32:49 -0500

    Ajustes de visualizacion

--- Coldairarrow.Business/Base_SysManage/HomeBusiness.cs
+++ Coldairarrow.Business/Base_SysManage/HomeBusiness.cs
@@ -10,7 +10,7 @@ namespace Coldairarrow.Business.Base_SysManage
         public AjaxResult SubmitLogin(string userName, string password)
         {
             if (userName.IsNullOrEmpty() || password.IsNullOrEmpty())
-                return Error("User name or password can not be empty!");
+                return Error("Debe digitar el Usuario y la Clave!");
             password = password.ToMD5String();
             var theUser = GetIQueryable().Where(x => x.UserName == userName && x.Password == password).FirstOrDefault();
             if (theUser != null)
@@ -19,7 +19,7 @@ namespace Coldairarrow.Business.Base_SysManage
                 return Success();
             }
             else
-                return Error("Invalid user name or password!");
+                return Error("Usuario o Clave incorrectas!");
         }
     }
 }
